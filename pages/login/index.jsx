46a70dd26112094bf81d921f2d9906a676a0727a
a0p0dp0 <armando.paulo0@walmart.com>
2023-10-09 23:28:56 -0300

    :tada: feat: added initial login page

---
+++ pages/login/index.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+const Login = () => {
+    return (
+        <div className={styles.container}>
+            <h1>LOGIN PAGE (NextJs)</h1>
+        </div>
+    )
+}
+
+export default Login
