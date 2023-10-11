25ecd391bc77159a6b4e6345e47649e690a59604
a0p0dp0 <armando.paulo0@walmart.com>
2023-10-10 22:03:44 -0300

    :lipstick: feat: added styles to login form component

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,10 +1,26 @@
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 import styles from './styles.module.css'
 
 const Login = () => {
     return (
-        <div className={styles.container}>
-            <h1>LOGIN PAGE (NextJs)</h1>
-        </div>
+        <Stack
+            className={styles.container}
+            component="form"
+            sx={{
+                width: '40ch',
+            }}
+            spacing={2}
+            noValidate
+            autoComplete="off"
+        >
+            <TextField label="Username or Email" variant="outlined"/>
+            <TextField label="Password" type="password" variant="outlined"/>
+            <Button variant="contained" color="primary">Login</Button>
+        </Stack>
+
     )
 }
 
